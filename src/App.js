04add1100abb1cc9f0b72aaf69da74278a138607
Faith Kariuki <tcwamboi@gmail.com>
2024-08-08 10:47:29 +0300

    Complete lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import image from "../src/asset/programmer_icon.svg";
+
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+    <h1>Hi, I'm Faith</h1>
+    <img src={image} alt="Portfolio image" />
+    <h2>About Me</h2>
+    <p>I'm a software engineer who loves to code</p>
+    <a href="https://github.com/Faith-K-commits">GitHub</a>
+    <a href="https://www.linkedin.com/in/faith-kariuki-3b0556210/">Linkedin</a>
+  </div>;
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,4 +3,60 @@ import '@testing-library/jest-dom';
 
 import App from "../App";
 
-// Your tests here
+test("displays a top-level heading with the text `Hi, I'm ______`", () => {
+    render(<App />);
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1
+    })
+    expect(topLevelHeading).toBeInTheDocument();
+});
+
+// Test that a portfolio image is present
+
+test("displays a portfolio image", () => {
+    render(<App />)
+    const portfolioImage = screen.getByAltText("Portfolio image", {
+        exact: false,
+    })
+    expect(portfolioImage).toBeInTheDocument();
+})
+
+// Test about me heading
+test("displays a second-level heading with the text `About me`", () => {
+    render(<App />)
+    const secondLevelHeading = screen.getByRole("heading", {
+        name: /about me/i,
+        exact: false,
+        level: 2
+    })
+    expect(secondLevelHeading).toBeInTheDocument();
+})
+
+// Test about me paragraph
+test("displays a paragraph with the text `I'm a software engineer who loves to code`", () => {
+    render(<App />)
+    const paragraph = screen.getByText(/I'm a software engineer who loves to code/i)
+    expect(paragraph).toBeInTheDocument();
+})
+
+// Test github link is present
+test("displays a link to my GitHub profile", () => {
+    render(<App />)
+    const githubLink = screen.getByRole("link", {
+        name: /github/i,
+        exact: false
+    })
+    expect(githubLink).toBeInTheDocument();
+})
+
+// Test linkdln link is present
+test("displays a link to my LinkedIn profile", () => {
+    render(<App />)
+    const linkedInLink = screen.getByRole("link", {
+        name: /linkedin/i,
+        exact: false
+    })
+    expect(linkedInLink).toBeInTheDocument();
+})
